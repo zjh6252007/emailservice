ac2912e686a6c02d93f8339923294181d3a4e404
Jay <zjh6252007@gmail.com>
2024-04-12 22:14:23 -0400

    Create email service

---
+++ src/main/java/com/jay/emailservice/EmailserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jay.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailserviceApplication.class, args);
+    }
+
+}
